7612c26cf88607b8859a4c38e463641358865ef7
David Siegel <david.siegel@artcom.de>
2013-03-07 14:50:42 +0100

    added avatar bezel layer and a dummy asset

--- Hoccenger/Views/AvatarBezelView.m
+++ Hoccenger/Views/AvatarBezelView.m
@@ -18,8 +18,16 @@ static const double kCornerRadius = 3.0;
     [self.layer setMasksToBounds: YES];
     self.layer.cornerRadius = kCornerRadius;
 
+    /*
     self.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.layer.borderWidth = 1.0;
+    */
+
+    CALayer * layer = [CALayer layer];
+    layer.contents = (id)[UIImage imageNamed: @"avatar_bezel"].CGImage;
+
+    layer.frame = self.bounds;
+    [self.layer insertSublayer: layer atIndex: 0];
 }
 
 - (void) setImage:(UIImage *)image {
