9ceb3548c71c6650ffc4d3a25aaad86159dd3cdf
David Siegel <david.siegel@artcom.de>
2013-03-16 07:01:41 +0100

    added the hoccer logo to the navigation bar

--- HoccerTalk/ContactListViewController.m
+++ HoccerTalk/ContactListViewController.m
@@ -52,6 +52,8 @@
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithImage: icon style:UIBarButtonItemStylePlain target:nil action:nil];
     self.navigationItem.backBarButtonItem = backButton;
 
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage: [UIImage imageNamed: @"navbar_logo"]];
+
     self.chatViewController = (ChatViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
