8c3861271785efae69a9e99a89f298a834b0fb8b
David Siegel <david.siegel@artcom.de>
2013-04-02 17:08:10 +0200

    just for zutrinken: center navigation buttons in navigation bar fields

--- HoccerTalk/Views/CustomNavigationBar.m
+++ HoccerTalk/Views/CustomNavigationBar.m
@@ -9,6 +9,12 @@
 #import "CustomNavigationBar.h"
 
 #import <QuartzCore/QuartzCore.h>
+#import <UIKit/UINavigationBar.h>
+
+static const CGFloat kButtonWidth = 49;
+static const CGFloat kButtonColumnWidth = 63;
+static const CGFloat kButtonXOffset = 0.5 * (kButtonColumnWidth - kButtonWidth);
+
 
 @implementation CustomNavigationBar
 
@@ -39,11 +45,22 @@
     self.layer.shadowPath = [UIBezierPath bezierPathWithRect: shadowRect].CGPath;
 
     CGRect maskRect = self.bounds;
-    maskRect.size.height += 20;
+    maskRect.size.height += 5 + 2 * self.layer.shadowRadius;
     _mask.path = [UIBezierPath bezierPathWithRect: maskRect].CGPath;
 
+    CGFloat screenWidth = UIDeviceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation) ? [UIScreen mainScreen].applicationFrame.size.width : [UIScreen mainScreen].applicationFrame.size.height;
+
     for (UIView * subview in self.subviews) {
-        NSLog(@"subview: %@", subview);
+        if ([NSStringFromClass([subview class]) isEqualToString: @"UINavigationButton"]) {
+            CGRect frame = subview.frame;
+            frame.size.width = kButtonWidth;
+            if (frame.origin.x > 0.5 * screenWidth ) {
+                frame.origin.x = (screenWidth - kButtonColumnWidth) + kButtonXOffset;
+            } else {
+                frame.origin.x = kButtonXOffset;
+            }
+            subview.frame = frame;
+        }
     }
 }
 
