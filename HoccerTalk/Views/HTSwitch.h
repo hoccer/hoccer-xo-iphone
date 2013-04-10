67f726536d4d139d6dd351adabc85398389958e2
David Siegel <david.siegel@artcom.de>
2013-04-10 07:47:38 +0200

    abusing a slider to build @zutrinken's setting switch

---
+++ HoccerTalk/Views/HTSwitch.h
@@ -0,0 +1,15 @@
+//
+//  HTSwitch.h
+//  HoccerTalk
+//
+//  Created by David Siegel on 10.04.13.
+//  Copyright (c) 2013 Hoccer GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTSwitch : UISlider
+
+@property (nonatomic,assign) BOOL boolValue;
+
+@end
