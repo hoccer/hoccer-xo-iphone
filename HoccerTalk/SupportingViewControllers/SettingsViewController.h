e1f0afc7364236819c45f00b039f1df0ba08da56
David Siegel <david.siegel@artcom.de>
2013-04-10 03:16:19 +0200

    started to implement the settings view

--- HoccerTalk/SupportingViewControllers/SettingsViewController.h
+++ HoccerTalk/SupportingViewControllers/SettingsViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface SettingsViewController : UITableViewController
+{
+    NSArray * _items;
+}
 
 @end
--- HoccerTalk/SupportingViewControllers/SettingsViewController.m
+++ HoccerTalk/SupportingViewControllers/SettingsViewController.m
@@ -7,7 +7,19 @@
 //
 
 #import "SettingsViewController.h"
+
 #import "UIViewController+HoccerTalkSideMenuButtons.h"
+#import "RadialGradientView.h"
+#import "SettingsCells.h"
+
+@interface SettingsItem : NSObject
+
+@property (nonatomic,strong) NSString * cellIdentifier;
+@property (nonatomic,strong) NSString * label;
+
++ (SettingsItem*) item;
+
+@end
 
 @implementation SettingsViewController
 
@@ -16,11 +28,55 @@
 
     self.navigationItem.leftBarButtonItem = [self hoccerTalkMenuButton];
     self.navigationItem.rightBarButtonItem = [self hoccerTalkContactsButton];
+
+    self.tableView.backgroundView = [[RadialGradientView alloc] initWithFrame: self.tableView.frame];
+
+    [self populateSettingsItem];
 }
 
 - (void) viewWillAppear:(BOOL)animated  {
     [super viewWillAppear: animated];
-    [self setNavigationBarBackgroundWithLines];
+    [self setNavigationBarBackgroundPlain];
+}
+
+- (void) populateSettingsItem {
+    SettingsItem * saveContent = [SettingsItem item];
+    saveContent.cellIdentifier = [SettingSwitchCell reuseIdentifier];
+    saveContent.label = NSLocalizedString(@"setting_save_incoming_media", nil);
+
+    SettingsItem * saveContentInfo = [SettingsItem item];
+    saveContentInfo.cellIdentifier = [SettingTextCell reuseIdentifier];
+    saveContentInfo.label = NSLocalizedString(@"setting_save_incoming_media_info", nil);
+    
+    _items = @[ @[saveContent, saveContentInfo ]
+              ];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return _items.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [_items[section] count];
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44.0; //XXX
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    SettingsItem * item = _items[indexPath.section][indexPath.row];
+    UITableViewCell * cell = [self.tableView dequeueReusableCellWithIdentifier: item.cellIdentifier];
+    cell.textLabel.text = item.label;
+    return cell;
+}
+
+
+@end
+
+@implementation SettingsItem
+
++ (SettingsItem*) item {
+    return [[SettingsItem alloc] init];
 }
 
 @end
