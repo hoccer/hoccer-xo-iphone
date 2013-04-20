ba4e95c278b54048e555eb5f87adc7a74ed4f34e
David Siegel <david.siegel@artcom.de>
2013-04-20 17:24:16 +0200

    updated submodules

---
+++ HoccerTalk/UserProfile.h
@@ -0,0 +1,13 @@
+//
+//  UserProfile.h
+//  HoccerTalk
+//
+//  Created by David Siegel on 20.04.13.
+//  Copyright (c) 2013 Hoccer GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserProfile : NSObject
+
+@end
---
+++ HoccerTalk/UserProfile.m
@@ -0,0 +1,13 @@
+//
+//  UserProfile.m
+//  HoccerTalk
+//
+//  Created by David Siegel on 20.04.13.
+//  Copyright (c) 2013 Hoccer GmbH. All rights reserved.
+//
+
+#import "UserProfile.h"
+
+@implementation UserProfile
+
+@end
