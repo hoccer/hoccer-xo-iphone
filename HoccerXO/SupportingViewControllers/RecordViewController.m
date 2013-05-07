ae7aab601612c1410ece505e7326cc54c399d14b
Pavel Mayer <pavel@hoccer.com>
2013-05-07 03:17:14 +0200

    Audio Recorder cleanup, visualized button status

--- HoccerXO/SupportingViewControllers/RecordViewController.m
+++ HoccerXO/SupportingViewControllers/RecordViewController.m
@@ -12,16 +12,21 @@
 #import "TDSemiModal.h"
 
 @interface RecordViewController ()
-
 @end
 
 @implementation RecordViewController
-
+{
+    CGFloat disabledAlpha;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _playButton.enabled = NO;
-    _stopButton.enabled = NO;
+    
+    disabledAlpha = 0.1;
+    
+    [self disablePlay];
+    [self disableStop];
+    [self enableRecord];
     
     NSDictionary *recordSettings = @{AVEncoderAudioQualityKey : @(AVAudioQualityMin),
                                      AVEncoderBitRateKey : @(16),
@@ -42,58 +47,10 @@
     } else {
         [_audioRecorder prepareToRecord];
     }
-    NSLog(@"Audiorecorder prepared, URL: %@", self.audioFileURL);
+    // NSLog(@"Audiorecorder prepared, URL: %@", self.audioFileURL);
     [self updateStatusDisplay];
     [self updateTimeDisplay:nil];
 }
-
-- (void)viewWillAppear:(BOOL)animated {
-	//slide in the filter view from the bottom
-	// [self slideIn];
-    NSLog(@"viewWillAppear");
-}
-
-- (void)slideIn {
-	
-	//set initial location at bottom of view
-    CGRect frame = self.sheetView.frame;
-    frame.origin = CGPointMake(0.0, self.view.bounds.size.height);
-    self.sheetView.frame = frame;
-    [self.view addSubview:self.sheetView];
-	
-    //animate to new location, determined by height of the view in the NIB
-    [UIView beginAnimations:@"presentWithSuperview" context:nil];
-    frame.origin = CGPointMake(0.0, self.view.bounds.size.height - self.sheetView.bounds.size.height);
-	
-    self.sheetView.frame = frame;
-    [UIView commitAnimations];
-}
-
-- (void) slideOut {
-	
-	//do what you need to do with information gathered from the custom action sheet. E.g., apply data filter on parent view.
-	
-	[UIView beginAnimations:@"removeFromSuperviewWithAnimation" context:nil];
-	
-    // Set delegate and selector to remove from superview when animation completes
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
-	
-    // Move this view to bottom of superview
-    CGRect frame = self.sheetView.frame;
-    frame.origin = CGPointMake(0.0, self.view.bounds.size.height);
-    self.sheetView.frame = frame;
-	
-    [UIView commitAnimations];
-}
-
-// Method called when removeFromSuperviewWithAnimation's animation completes
-- (void)animationDidStop:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context {
-    if ([animationID isEqualToString:@"removeFromSuperviewWithAnimation"]) {
-        [self.view removeFromSuperview];
-    }
-}
-
 
 - (void)didReceiveMemoryWarning
 {
@@ -124,14 +81,44 @@
 }
 
 - (void) startTimer {
-    NSLog(@"startTimer:");
+    // NSLog(@"startTimer:");
     self.updateTimer = [NSTimer scheduledTimerWithTimeInterval:0.3 target:self selector:@selector(updateTimeDisplay:) userInfo:nil repeats:YES];
 }
 
 - (void) stopTimer {
-    NSLog(@"stopTimer:");
+    // NSLog(@"stopTimer:");
     [self.updateTimer invalidate];
     self.updateTimer = nil;
+}
+
+- (void)enableStop {
+    _stopButton.enabled = YES;
+    _stopButton.alpha = 1.0;;
+}
+
+- (void)enablePlay {
+    _playButton.enabled = YES;
+    _playButton.alpha = 1.0;;
+}
+
+- (void)enableRecord {
+    _recordButton.enabled = YES;
+    _recordButton.alpha = 1.0;;
+}
+
+- (void)disableStop {
+    _stopButton.enabled = NO;
+    _stopButton.alpha = disabledAlpha;;
+}
+
+- (void)disablePlay {
+    _playButton.enabled = NO;
+    _playButton.alpha = disabledAlpha;;
+}
+
+- (void)disableRecord {
+    _recordButton.enabled = NO;
+    _recordButton.alpha = disabledAlpha;;
 }
 
 - (void) updateTimeDisplay:(NSTimer *)theTimer {
@@ -148,30 +135,30 @@
 }
 
 - (IBAction)recordAudio:(id)sender {
-    NSLog(@"recordAudio:");
+    // NSLog(@"recordAudio:");
     if (!_audioRecorder.recording)
     {
-        _playButton.enabled = NO;
-        _stopButton.enabled = YES;
+        [self disablePlay];
+        [self enableStop];
         // [_audioRecorder deleteRecording];
         AVAudioSession *session = [AVAudioSession sharedInstance];
         [session setCategory:AVAudioSessionCategoryRecord error:nil];
         [session setActive:YES error:nil];
         [_audioRecorder record];
         [self startTimer];
-        NSLog(@"Audiorecorder record: %d", _audioRecorder.recording);
+        // NSLog(@"Audiorecorder record: %d", _audioRecorder.recording);
     }
     [self updateStatusDisplay];
 }
 
 - (IBAction)playAudio:(id)sender {
     if (!_audioRecorder.recording) {
-        _stopButton.enabled = YES;
-        _recordButton.enabled = NO;
+        [self disableRecord];
+        [self enableStop];
         
         NSError *error;
         
-        NSLog(@"Audiorecorder init url: %@", _audioRecorder.url);
+        // NSLog(@"Audiorecorder init url: %@", _audioRecorder.url);
         AVAudioSession *session = [AVAudioSession sharedInstance];
         [session setCategory:AVAudioSessionCategoryPlayback error:nil];
         [session setActive:YES error:nil];
@@ -184,7 +171,7 @@
         if (error) {
             NSLog(@"Error: %@", [error localizedDescription]);
         } else {
-            NSLog(@"Audioplayer play: %@", _audioRecorder.url);
+            // NSLog(@"Audioplayer play: %@", _audioRecorder.url);
             [_audioPlayer play];
             [self startTimer];
         }
@@ -193,9 +180,9 @@
 }
 
 - (IBAction)stop:(id)sender {
-    _stopButton.enabled = NO;
-    _playButton.enabled = YES;
-    _recordButton.enabled = YES;
+    [self enableRecord];
+    [self enablePlay];
+    [self disableStop];
     
     if (_audioRecorder.recording) {
         [self stopTimer];
@@ -203,62 +190,44 @@
         AVAudioSession *session = [AVAudioSession sharedInstance];
         int flags = AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation;
         [session setActive:NO withOptions:flags error:nil];
-        NSLog(@"_audioRecorder stopped");
+        // NSLog(@"_audioRecorder stopped");
     } else if (_audioPlayer.playing) {
         AVAudioSession *session = [AVAudioSession sharedInstance];
         int flags = AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation;
         [session setActive:NO withOptions:flags error:nil];
         [self stopTimer];
         [_audioPlayer stop];
-        NSLog(@"_audioPlayer stopped");
+        // NSLog(@"_audioPlayer stopped");
     }
     [self updateStatusDisplay];
 }
 
 #pragma mark -- Navigation action methods
 
-#ifdef PRESENTED_VIEW
-- (IBAction)usePressed:(id)sender {
-    NSLog(@"usePressed:");
-    [self dismissViewControllerAnimated: YES completion: nil];
-    if (self.delegate != nil) {
-        NSLog(@"usePressed calling didRecordAudio %@",self.audioFileURL);
-        [self.delegate audiorecorder:self didRecordAudio:self.audioFileURL];
-    }
-}
 
-- (IBAction)cancelPressed:(id)sender {
-    [_audioRecorder deleteRecording];
-    [self dismissViewControllerAnimated: YES completion: nil];
-}
-#else
 - (IBAction)usePressed:(id)sender {
-    NSLog(@"usePressed:");
-    //[self dismissViewControllerAnimated: YES completion: nil];
-    // [self.view removeFromSuperview];
+    // NSLog(@"usePressed:");
 	[self dismissSemiModalViewController:self];
     
     if (self.delegate != nil) {
-        NSLog(@"usePressed calling didRecordAudio %@",self.audioFileURL);
+        // NSLog(@"usePressed calling didRecordAudio %@",self.audioFileURL);
         [self.delegate audiorecorder:self didRecordAudio:self.audioFileURL];
     }
 }
 
 - (IBAction)cancelPressed:(id)sender {
     [_audioRecorder deleteRecording];
-    // [self dismissViewControllerAnimated: YES completion: nil];
-    // [self.view removeFromSuperview];
 	[self dismissSemiModalViewController:self];
 
 }
-
-#endif
-
 
 #pragma mark -- delegate methods
 
 -(void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
 {
+    [self enableRecord];
+    [self enablePlay];
+    [self disableStop];
     _recordButton.enabled = YES;
     _stopButton.enabled = NO;
     [self updateStatusDisplay];
@@ -273,7 +242,7 @@
 
 -(void)audioRecorderDidFinishRecording: (AVAudioRecorder *)recorder successfully:(BOOL)flag
 {
-    NSLog(@"audioRecorderDidFinishRecording, successfully=%d", flag);
+    // NSLog(@"audioRecorderDidFinishRecording, successfully=%d", flag);
     [self updateStatusDisplay];
 }
 
