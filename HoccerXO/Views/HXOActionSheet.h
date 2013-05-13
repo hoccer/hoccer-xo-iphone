80ebe85d3a3d59c01f356e5992286a50c22a5cd4
David Siegel <david.siegel@artcom.de>
2013-05-13 20:32:42 +0200

    merged build number

--- HoccerXO/Views/HXOActionSheet.h
+++ HoccerXO/Views/HXOActionSheet.h
@@ -8,6 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-@interface HXOActionSheet : NSObject
+@interface HXOActionSheet : UIView
+{
+    NSMutableArray * _buttonTitles;
+}
+
+@property (nonatomic,assign) id<UIActionSheetDelegate> delegate;
+@property (nonatomic,copy) NSString * title;
+
+@property (nonatomic) NSInteger cancelButtonIndex;
+@property (nonatomic) NSInteger destructiveButtonIndex;
+@property (nonatomic,readonly) NSInteger firstOtherButtonIndex;
+@property (nonatomic,readonly) NSInteger numberOfButtons;
+
+- (id)initWithTitle:(NSString *)title delegate:(id < UIActionSheetDelegate >)delegate cancelButtonTitle:(NSString *)cancelButtonTitle destructiveButtonTitle:(NSString *)destructiveButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...;
+
+- (NSInteger) addButtonWithTitle: (NSString*) title;
+- (NSString *)buttonTitleAtIndex:(NSInteger)buttonIndex;
+- (void)showInView:(UIView *)view;
 
 @end
--- HoccerXO/Views/HXOActionSheet.m
+++ HoccerXO/Views/HXOActionSheet.m
@@ -10,4 +10,53 @@
 
 @implementation HXOActionSheet
 
+- (id) initWithTitle:(NSString *)title delegate:(id < UIActionSheetDelegate >)delegate cancelButtonTitle:(NSString *)cancelButtonTitle destructiveButtonTitle:(NSString *)destructiveButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...
+{
+    self = [super init];
+    if (self != nil) {
+        self.title = title;
+        self.delegate = delegate;
+        self.destructiveButtonIndex = -1;
+        self.cancelButtonIndex = -1;
+        _firstOtherButtonIndex = -1;
+        _buttonTitles = [[NSMutableArray alloc] init];
+        if (destructiveButtonTitle != nil) {
+            self.destructiveButtonIndex = 0;
+            [_buttonTitles addObject: destructiveButtonTitle];
+        }
+        va_list otherButtons;
+        NSString * title;
+        while ((title = va_arg(otherButtons, NSString * ))) {
+            if (_firstOtherButtonIndex == -1) {
+                _firstOtherButtonIndex = _buttonTitles.count;
+            }
+            [_buttonTitles addObject: title];
+        }
+        va_end(otherButtons);
+        if (cancelButtonTitle != nil) {
+            self.cancelButtonIndex = _buttonTitles.count;
+            [_buttonTitles addObject: cancelButtonTitle];
+        }
+    }
+    return self;
+}
+
+- (NSInteger) addButtonWithTitle: (NSString*) title {
+    NSInteger index = _buttonTitles.count;
+    [_buttonTitles addObject: title];
+    return index;
+}
+
+- (NSInteger) numberOfButtons {
+    return _buttonTitles.count;
+}
+
+- (NSString *)buttonTitleAtIndex:(NSInteger)buttonIndex {
+    return _buttonTitles[buttonIndex];
+}
+
+- (void)showInView:(UIView *)view {
+
+}
+
 @end
