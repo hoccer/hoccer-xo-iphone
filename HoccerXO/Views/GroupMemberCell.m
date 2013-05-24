3219b20d02be27a9225ad42da87c53e530e9ba7f
David Siegel <david.siegel@artcom.de>
2013-05-24 16:26:44 +0200

    force group member cells to full width

--- HoccerXO/Views/GroupMemberCell.m
+++ HoccerXO/Views/GroupMemberCell.m
@@ -8,6 +8,8 @@
 
 #import "GroupMemberCell.h"
 
+
+
 @implementation GroupMemberCell
 
 - (void) configureBackgroundViewForPosition: (NSUInteger) position inSectionWithCellCount: (NSUInteger) cellCount {
@@ -16,5 +18,18 @@
 - (void) awakeFromNib {
     [super awakeFromNib];
     self.editingAccessoryView = self.accessoryView;
+}
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+    CGRect frame = self.backgroundView.frame;
+    CGFloat padding = frame.origin.x;
+    frame.origin.x = 0;
+    frame.size.width += 2 * padding;
+    self.backgroundView.frame = frame;
+    frame = self.contentView.frame;
+    frame.origin.x = 0;
+    frame.size.width += 2 * padding;
+    self.contentView.frame = frame;
 }
 @end
