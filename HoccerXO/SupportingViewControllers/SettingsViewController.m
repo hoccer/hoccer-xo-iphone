501d8042c2b60e314d7fad11d225795f07051640
David Siegel <david.siegel@artcom.de>
2014-02-05 20:10:55 +0100

    Use upstream vanilla InAppSettingsKit instead of iOS6 XO themed version

--- HoccerXO/SupportingViewControllers/SettingsViewController.m
+++ HoccerXO/SupportingViewControllers/SettingsViewController.m
@@ -13,11 +13,6 @@
 #import "UIViewController+HXOSideMenu.h"
 #import "UserDefaultsCells.h"
 
-
-@interface HXOSpecifierValuesViewController : IASKSpecifierValuesViewController
-
-@end
-
 @implementation SettingsViewController
 
 - (void) viewDidLoad {
@@ -33,32 +28,8 @@
     [super viewWillAppear: animated];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell * cell = [super tableView: tableView cellForRowAtIndexPath: indexPath];
-    [UserDefaultsCell configureGroupedCell: cell forPosition: indexPath.row inSectionWithCellCount: [self tableView: tableView numberOfRowsInSection: indexPath.section]];
-    cell.textLabel.backgroundColor = [UIColor clearColor];
-    cell.detailTextLabel.backgroundColor = [UIColor clearColor];
-    return cell;
-}
-
-- (IASKSpecifierValuesViewController*) specifierValuesViewControllerForSettingsViewController:(IASKAppSettingsViewController *)sender {
-    return [[HXOSpecifierValuesViewController alloc] initWithTableClass: sender.tableView.class];
-}
-
 - (void)settingsViewControllerDidEnd:(IASKAppSettingsViewController*)sender {
     
-}
-
-@end
-
-@implementation HXOSpecifierValuesViewController
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell * cell = [super tableView: tableView cellForRowAtIndexPath: indexPath];
-    [UserDefaultsCell configureGroupedCell: cell forPosition: indexPath.row inSectionWithCellCount: [self tableView: tableView numberOfRowsInSection: indexPath.section]];
-    cell.textLabel.backgroundColor = [UIColor clearColor];
-    cell.detailTextLabel.backgroundColor = [UIColor clearColor];
-    return cell;
 }
 
 @end
