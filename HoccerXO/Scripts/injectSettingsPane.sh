898ddb37e91fd6ef7e4d4b91d3118d3749a43bdf
David Siegel <david.siegel@artcom.de>
2014-02-13 16:15:06 +0100

    Work around build dependency issues when injecting debug settings pane. Closes #220.

--- HoccerXO/Scripts/injectSettingsPane.sh
+++ HoccerXO/Scripts/injectSettingsPane.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-#  injectDebugSettingsPane.sh
+#  injectSettingsPane.sh
 #  HoccerXO
 #
 #  Created by David Siegel on 12.02.14.
 #  Copyright (c) 2014 Hoccer GmbH. All rights reserved.
 
-PROG="`basename $0`"
-PLISTBUDDY="/usr/libexec/PlistBuddy -x"
+PROG=`basename $0`
+PLISTBUDDY="/usr/libexec/PlistBuddy"
 
-set -e
+#set -e
 
 if [ -z "$1" ]; then
   echo "Usage:"
   echo "   $PROG plist_file [child_pane_name]"
-  echo "   - If unspecified, child_pane_name is 'Debug'"
+  echo "   child_pane_name defaults to 'Debug'"
   exit 1
 fi
 
@@ -30,12 +30,19 @@ else
 fi
 
 TARGET="$1"
-echo "[$PROG] adding '$CHILD_PANE_NAME' child to: $TARGET"
+
+CHILD_PANE_FILE_VALUE=`$PLISTBUDDY -c "Print PreferenceSpecifiers:1:File" "$TARGET" 2>/dev/null`
+if [ $? -eq 0 ] && [ "$CHILD_PANE_FILE_VALUE" == "$CHILD_PANE_NAME" ]; then
+  echo "$PROG: child pane '$CHILD_PANE_NAME' is already present"
+  exit 0
+fi
+
+echo "$PROG: adding child pane '$CHILD_PANE_NAME' to $TARGET"
 
 $PLISTBUDDY -c "Add PreferenceSpecifiers:0 dict" "$TARGET"
 $PLISTBUDDY -c "Add PreferenceSpecifiers:0:Type string 'PSGroupSpecifier'" "$TARGET"
 
 $PLISTBUDDY -c "Add PreferenceSpecifiers:1 dict" "$TARGET"
 $PLISTBUDDY -c "Add PreferenceSpecifiers:1:Type string 'PSChildPaneSpecifier'" "$TARGET"
-$PLISTBUDDY -c "Add PreferenceSpecifiers:1:Title string '$CHILD_PANE_NAME Settings'" "$TARGET"
+$PLISTBUDDY -c "Add PreferenceSpecifiers:1:Title string '$CHILD_PANE_NAME'" "$TARGET"
 $PLISTBUDDY -c "Add PreferenceSpecifiers:1:File string '$CHILD_PANE_NAME'" "$TARGET"
