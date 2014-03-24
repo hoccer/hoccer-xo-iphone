b8019a5ebe484d5511508ba7bd8522dfa44cba6c
David Siegel <david.siegel@artcom.de>
2014-03-24 16:46:10 +0100

    Removed stall files and added a new data sheet view controller

---
+++ HoccerXO/SupportingViewControllers/DataSheetViewController.h
@@ -0,0 +1,13 @@
+//
+//  DataSheetViewController.h
+//  HoccerXO
+//
+//  Created by David Siegel on 21.03.14.
+//  Copyright (c) 2014 Hoccer GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataSheetViewController : UIViewController
+
+@end
