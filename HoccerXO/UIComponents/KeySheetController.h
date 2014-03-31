451c597b3c6e4d0a06e186d2d4af1b47fe4c9b4c
David Siegel <david.siegel@artcom.de>
2014-03-31 20:05:51 +0200

    Added a stub key view.

---
+++ HoccerXO/UIComponents/KeySheetController.h
@@ -0,0 +1,13 @@
+//
+//  KeySheetController.h
+//  HoccerXO
+//
+//  Created by David Siegel on 31.03.14.
+//  Copyright (c) 2014 Hoccer GmbH. All rights reserved.
+//
+
+#import "DatasheetController.h"
+
+@interface KeySheetController : DatasheetController
+
+@end
---
+++ HoccerXO/UIComponents/KeySheetController.m
@@ -0,0 +1,13 @@
+//
+//  KeySheetController.m
+//  HoccerXO
+//
+//  Created by David Siegel on 31.03.14.
+//  Copyright (c) 2014 Hoccer GmbH. All rights reserved.
+//
+
+#import "KeySheetController.h"
+
+@implementation KeySheetController
+
+@end
