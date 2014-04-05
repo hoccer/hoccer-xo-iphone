50ad168540f77c6f63bee9e2f91559e104a4a2d5
David Siegel <david.siegel@artcom.de>
2014-04-06 00:41:09 +0200

    Tweaked my paper clip and added paper dart by @zutrinken.

--- HoccerXO/VectorArt/PaperClip.m
+++ HoccerXO/VectorArt/PaperClip.m
@@ -10,12 +10,12 @@
 
 @implementation PaperClip
 
-static const CGFloat kStartX = 20.5;
+static const CGFloat kStartX = 18.5;
 static const CGFloat kBigRadius = 6;
-static const CGFloat kSecondBendX = 23.5;
-static const CGFloat kSmallRadius = 2.5;
+static const CGFloat kSecondBendX = 22.5;
+static const CGFloat kSmallRadius = 3;
 static const CGFloat kSecondBendRadius = (kBigRadius + kSmallRadius) / 2;
-static const CGFloat kInnerBendX = 10.5;
+static const CGFloat kInnerBendX = 12.5;
 static const CGFloat kInnerEndX = kStartX;
 
 
@@ -26,8 +26,8 @@ static const CGFloat kInnerEndX = kStartX;
     [self.path addLineToPoint: CGPointMake(kSecondBendX, 2 * kBigRadius)];
     [self.path addArcWithCenter: CGPointMake(kSecondBendX, 2 * kBigRadius - kSecondBendRadius) radius: kSecondBendRadius startAngle: 0.5 * M_PI endAngle:  1.5 * M_PI clockwise: NO];
     [self.path addLineToPoint: CGPointMake(kInnerBendX, 2 * (kBigRadius - kSecondBendRadius))];
-    [self.path addArcWithCenter: CGPointMake(kInnerBendX,  kBigRadius) radius: kSmallRadius startAngle: 1.5 * M_PI endAngle:  0.5 * M_PI clockwise: NO];
-    [self.path addLineToPoint: CGPointMake(kInnerEndX, kBigRadius + kSmallRadius)];
+    [self.path addArcWithCenter: CGPointMake(kInnerBendX,  kBigRadius + .25) radius: kSmallRadius + .25 startAngle: 1.5 * M_PI endAngle:  0.5 * M_PI clockwise: NO];
+    [self.path addLineToPoint: CGPointMake(kInnerEndX, kBigRadius + kSmallRadius + .5)];
 
     self.strokeColor = [UIColor blackColor];
     self.path.lineWidth = 1.0;
--- HoccerXO/VectorArt/PaperDart.m
+++ HoccerXO/VectorArt/PaperDart.m
@@ -13,19 +13,19 @@
 - (void) initPath {
     UIBezierPath * bezierPath = self.path;
 
-    [bezierPath moveToPoint: CGPointMake(18.5, 11.5)];
-    [bezierPath addLineToPoint: CGPointMake(11.5, 9.5)];
-    [bezierPath addLineToPoint: CGPointMake(6, 15)];
-    [bezierPath addLineToPoint: CGPointMake(6, 8)];
-    [bezierPath addLineToPoint: CGPointMake(0.5, 6.5)];
-    [bezierPath addLineToPoint: CGPointMake(21.5, 0.5)];
-    [bezierPath addLineToPoint: CGPointMake(18.5, 11.5)];
+    [bezierPath moveToPoint: CGPointMake(0, 8.1)];
+    [bezierPath addLineToPoint: CGPointMake(7, 9.1)];
+    [bezierPath addLineToPoint: CGPointMake(7.8, 18)];
+    [bezierPath addLineToPoint: CGPointMake(15.1, 11.8)];
+    [bezierPath addLineToPoint: CGPointMake(24.4, 15.1)];
+    [bezierPath addLineToPoint: CGPointMake(27, 0)];
+    [bezierPath addLineToPoint: CGPointMake(0, 8.1)];
     [bezierPath closePath];
-    [bezierPath moveToPoint: CGPointMake(18, 3)];
-    [bezierPath addLineToPoint: CGPointMake(7, 8)];
-    [bezierPath addLineToPoint: CGPointMake(7, 13)];
-    [bezierPath addLineToPoint: CGPointMake(9, 9)];
-    [bezierPath addLineToPoint: CGPointMake(18, 3)];
+    [bezierPath moveToPoint: CGPointMake(12.2, 9.9)];
+    [bezierPath addLineToPoint: CGPointMake(8.5, 16.4)];
+    [bezierPath addLineToPoint: CGPointMake(8.5, 9.1)];
+    [bezierPath addLineToPoint: CGPointMake(25.8, 1)];
+    [bezierPath addLineToPoint: CGPointMake(12.2, 9.9)];
     [bezierPath closePath];
 
     self.fillColor = [UIColor blackColor];
