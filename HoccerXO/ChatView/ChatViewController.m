2163be15a83fa41cc0dd40124265aedd88699994
David Siegel <david.siegel@artcom.de>
2014-05-05 17:07:07 +0200

    Fix #309: resign first responder while picking attachments

--- HoccerXO/ChatView/ChatViewController.m
+++ HoccerXO/ChatView/ChatViewController.m
@@ -511,7 +511,7 @@ typedef void(^AttachmentImageCompletion)(Attachment*, AttachmentSection*);
 
 - (IBAction)attachmentPressed: (id)sender {
     // NSLog(@"attachmentPressed");
-    //    [self.messageField resignFirstResponder];
+    [self.messageField resignFirstResponder]; // XXX :-/
     if (_currentPickInfo || _currentAttachment) {
         [self showAttachmentOptions];
     } else {
