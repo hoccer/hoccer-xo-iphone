b6aa5944826391415db902cefd84b396dc2f1336
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-02 10:01:14 +0200

    Fix compiler warning

--- HoccerXO/UtilityCategories/UIImage+Tint.m
+++ HoccerXO/UtilityCategories/UIImage+Tint.m
@@ -18,7 +18,7 @@
     CGRect bounds = CGRectMake(0,0,width,height);
     
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGContextRef bitmapContext = CGBitmapContextCreate(NULL, width, height, 8, 0, colorSpace, kCGImageAlphaPremultipliedLast);
+    CGContextRef bitmapContext = CGBitmapContextCreate(NULL, width, height, 8, 0, colorSpace, (CGBitmapInfo)kCGImageAlphaPremultipliedLast);
     CGContextClipToMask(bitmapContext, bounds, maskImage);
     CGContextSetFillColorWithColor(bitmapContext, color.CGColor);
     CGContextFillRect(bitmapContext, bounds);
