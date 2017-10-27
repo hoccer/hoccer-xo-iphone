55f258c1c427aa9b058f6918be263751ca1a5d35
David Siegel <agnat@me.com>
2017-10-27 17:27:26 +0200

    NSE: Initital checkin of notification service extension

---
+++ HoccerNSE/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  HoccerNSE
+//
+//  Created by David Siegel on 26.10.17.
+//  Copyright © 2017 Hoccer GmbH. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
